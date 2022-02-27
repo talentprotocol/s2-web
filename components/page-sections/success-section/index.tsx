95d80c1a0cea5f8c65d8a4a3f87656de7a6af30e
bguedes <bruno.guedes@mozantech.com>
2022-02-27 10:47:40 +0000

    feat: success section content

--- components/page-sections/success-section/index.tsx
+++ components/page-sections/success-section/index.tsx
@@ -1,10 +1,15 @@
 import Hero from "../../hero";
 import HeroCopy from "../../hero-copy";
+import notificationsImage from "atomic/_static/images/notifications.png";
 import { SuccessSection as COPY } from "copy/homepage";
+import {ContentContainer, StyledImage} from "./styled";
 
 const SuccessSection = () => (
   <Hero>
     <HeroCopy title={COPY.title} description={COPY.description} />
+      <ContentContainer>
+          <StyledImage src={notificationsImage} alt="talent protocol profile" />
+      </ContentContainer>
   </Hero>
 );
 
---
+++ components/page-sections/success-section/styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import Image from "next/image";
+
+export const ContentContainer = styled.div`
+  margin: 56px 0 0;
+`;
+
+export const StyledImage = styled(Image)`
+  width: 100%;
+`;
